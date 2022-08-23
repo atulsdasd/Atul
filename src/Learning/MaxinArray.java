400bfa7eebccb69d24f57de2e0a0a1862b4bcf93
Your Name <you@example.com>
2022-08-24 00:18:24 +0530

    changes done

--- src/Learning/MaxinArray.java
+++ src/Learning/MaxinArray.java
@@ -1,5 +1,12 @@
 package Learning;
 
 public class MaxinArray {
+public static void main(String[] args) {
+	 int[] arr = new int[] {4,3,2,1,5,6,7,43589453,-5};
+	 int sum=Integer.MIN_VALUE;
+	 for(int i=0;i<arr.length;i++)
+		sum=Math.max(sum, arr[i])	;
+		 System.out.println(sum);
 
+}
 }
