efae83fb40ed457552bb46a5d8dddfbfb2f1823c
Your Name <you@example.com>
2022-08-26 15:28:51 +0530

    one code

---
+++ src/Learning/starting.java
@@ -0,0 +1,20 @@
+package Learning;
+
+/**
+ * @author 93869
+ *
+ */
+public class starting {
+	public static void main(String[] args) {
+		int x = 15;
+		int y = 10;
+		int sum = x + y ;
+		System.out.print(sum);
+		//xy
+		int v1 = x + y;
+		int v2 = x * y;
+		int v3 = x / y;
+		System.out.print(v1 + v2 + v3);
+	}
+
+}
